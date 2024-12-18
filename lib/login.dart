12a851fb22e5992ef99eefe7a628e463f9ef9606
black-gigi <black-gigi@live.it>
2024-12-18 22:07:19 +0100

    aggiunta schermate : login.dart (login) , registration.dart (registrazione) e main.dart (landing page), aggiunta home.dart in cui c'è la schermata della home singleplayer (che avevamo in main.dart)

---
+++ lib/login.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'app_colors.dart';
+import 'home.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(const LoginPage());
+  });
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: AppColors.backgroundColor,
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
+
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Logo
+                Container(
+                  margin: EdgeInsets.only(
+                    top: screenHeight * 0.12,
+                    bottom: screenHeight * 0.01,
+                  ),
+                  child: Image.asset(
+                    'assets/logo_handup.png',
+                    width: screenWidth * 1,
+                    height: screenHeight * 0.35,
+                    fit: BoxFit.contain,
+                    errorBuilder: (context, error, stackTrace) {
+                      print('Error loading image: $error');
+                      return Icon(Icons.error, size: screenWidth * 0.8, color: Colors.red);
+                    },
+                  ),
+                ),
+
+                // Username TextField
+                Container(
+                  width: screenWidth * 0.8,
+                  // margin: EdgeInsets.symmetric(vertical: 10),
+                  margin: EdgeInsets.only(top: screenHeight * 0.01),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: 'Username',
+                      hintStyle: TextStyle(
+                        color: Colors.white.withOpacity(0.5),
+                        fontSize: screenWidth * 0.04,
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                    ),
+                  ),
+                ),
+
+                // Password TextField
+                Container(
+                  width: screenWidth * 0.8,
+                  // margin: EdgeInsets.symmetric(vertical: 10),
+                  margin: EdgeInsets.only(top: screenHeight * 0.025),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: TextField(
+                    obscureText: true,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      hintStyle: TextStyle(
+                        color: Colors.white.withOpacity(0.5),
+                        fontSize: screenWidth * 0.04,
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                    ),
+                  ),
+                ),
+
+                // Login Button
+                Container(
+                  margin: EdgeInsets.only(top: screenHeight * 0.025),
+                  width: screenWidth * 0.5,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppColors.textColor1.withOpacity(0.5),
+                        blurRadius: 20,
+                        spreadRadius: 2,
+                      ),
+                    ],
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(110, 214, 57, 196),
+                          Color.fromARGB(110, 255, 0, 208),
+                          Color.fromARGB(110, 140, 53, 232)
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        padding: EdgeInsets.symmetric(
+                          horizontal: screenWidth * 0.1,
+                          vertical: screenHeight * 0.015,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        elevation: 0,
+                        shadowColor: Colors.transparent,
+                      ),
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => const Home()),
+                        );
+                      },
+                      child: Text(
+                        'Entra',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: screenWidth * 0.045,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,14 @@ import "game_screen_spelling.dart";
 import "app_colors.dart";
 import "modalita_screen.dart";
 import 'package:google_fonts/google_fonts.dart';
+import 'login.dart';
+import 'registration.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
   ]).then((_) {
     runApp(const MyApp());
   });
@@ -17,23 +20,22 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-
       theme: ThemeData(
         brightness: Brightness.dark,
+        scaffoldBackgroundColor: AppColors.backgroundColor,
         textTheme: GoogleFonts.poppinsTextTheme(),
       ),
-      home: const HomeScreen(),
+      home: const LandingPageScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class LandingPageScreen extends StatelessWidget {
+  const LandingPageScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -41,281 +43,100 @@ class HomeScreen extends StatelessWidget {
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      drawer: SideMenu(),
-      body: Container(
-        decoration: const BoxDecoration(
-          color: AppColors.backgroundColor,
-        ),
-        child: SafeArea(
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return SingleChildScrollView(
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minHeight: constraints.maxHeight,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Logo container
+              Container(
+                margin: EdgeInsets.only(bottom: screenHeight * 0.05),
+                child: Image.asset(
+                  'assets/logo_handup.png',
+                  width: screenWidth * 1,
+                  height: screenHeight * 0.4,
+                  fit: BoxFit.contain,
+                  errorBuilder: (context, error, stackTrace) {
+                    print('Error loading image: $error');
+                    return Icon(Icons.error, size: screenWidth * 0.8, color: Colors.red);
+                  },
+                ),
+              ),
+              
+              // Start Now Button
+              Container(
+                width: screenWidth * 0.5,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColors.textColor1.withOpacity(0.5),
+                      blurRadius: 20,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(110, 214, 57, 196),
+                        Color.fromARGB(110, 255, 0, 208),
+                        Color.fromARGB(110, 140, 53, 232)
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                    borderRadius: BorderRadius.circular(30),
                   ),
-                  child: Column(
-                    children: [
-                      // App Bar
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Builder(builder: (BuildContext context) {
-                              return IconButton(
-                                icon: CustomPaint(
-                                  size: Size(24, 24),
-                                  painter: GradientIconPainter(
-                                    icon: Icons.menu,
-                                    gradient: AppColors.textGradient,
-                                  ),
-                                ),
-                                onPressed: () {
-                                  Scaffold.of(context).openDrawer();
-                                },
-                              );
-                            }),
-                            Row(
-                              children: [
-                                const CircleAvatar(
-                                  backgroundColor: Colors.purple,
-                                  child: Icon(Icons.person, color: Colors.white),
-                                ),
-                                const SizedBox(width: 8),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text(
-                                      'USERNAME',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white),
-                                    ),
-                                    Text(
-                                      'punti: 1000',
-                                      style: TextStyle(
-                                          color: Colors.grey[400], fontSize: 12),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: screenWidth * 0.1,
+                        vertical: screenHeight * 0.015,
                       ),
-
-                      // Main Content
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: Column(
-                          children: [
-                            // Top Card
-                            Container(
-                              padding: const EdgeInsets.fromLTRB(20, 20, 60, 30),
-                              decoration: BoxDecoration(
-                                color: Colors.transparent,
-                                border: Border.all(
-                                    color: AppColors.textColor2, width: 2),
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: Stack(
-                                clipBehavior: Clip.none,
-                                children: [
-                                  Positioned(
-                                    right: -screenWidth * 0.57,
-                                    bottom: -screenHeight * 0.66,
-                                    child: Image.asset(
-                                      'assets/hand_icon.png',
-                                      width: screenWidth * 1.5,
-                                      height: screenHeight * 1.5,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 0),
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        AppColors.gradientText(
-                                            "Allenati con parole casuali",
-                                            screenWidth * 0.07),
-                                        const SizedBox(height: 0),
-                                        Padding(
-                                          padding:
-                                              const EdgeInsets.only(right: 80),
-                                          child: Column(
-                                            children: [
-                                              Text(
-                                                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur imperdiet ex vel libero pharetra, vita a posuere purus egestas.',
-                                                style: TextStyle(
-                                                    color: Colors.grey[300]),
-                                              ),
-                                              const SizedBox(height: 20),
-                                              Align(
-                                                alignment: Alignment.centerLeft,
-                                                child: Container(
-                                                  decoration: BoxDecoration(
-                                                    borderRadius:
-                                                        BorderRadius.circular(30),
-                                                    boxShadow: [
-                                                      BoxShadow(
-                                                        color: Colors.pink
-                                                            .withOpacity(0.8),
-                                                        blurRadius: 20,
-                                                        spreadRadius: 4,
-                                                        offset:
-                                                            const Offset(0, 0),
-                                                      ),
-                                                    ],
-                                                  ),
-                                                  child: ElevatedButton(
-                                                      style: ElevatedButton.styleFrom(
-                                                        backgroundColor: Colors.pink,
-                                                        padding:
-                                                            const EdgeInsets
-                                                                .symmetric(
-                                                          horizontal: 15,
-                                                          vertical: 5,
-                                                        ),
-                                                        shape: RoundedRectangleBorder(
-                                                          borderRadius:
-                                                              BorderRadius
-                                                                  .circular(30),
-                                                        ),
-                                                        elevation: 0,
-                                                        shadowColor:
-                                                            Colors.transparent,
-                                                      ),
-                                                      onPressed: () {
-                                                        Navigator.push(
-                                                          context,
-                                                          MaterialPageRoute(
-                                                              builder: (context) =>
-                                                                  const ModalitaScreen()),
-                                                        );
-                                                      },
-                                                      child: const Padding(
-                                                          padding:
-                                                              EdgeInsets.symmetric(
-                                                            horizontal: 15,
-                                                            vertical: 8,
-                                                          ),
-                                                          child: Text(
-                                                            'Parole casuali',
-                                                          ))),
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            const SizedBox(height: 20),
-
-                            // Menu Items
-                            _buildMenuItem(
-                              title: 'Casa',
-                              imageUrl: 'assets/house_icon.png',
-                              isLocked: false,
-                              screenWidth: screenWidth,
-                              screenHeight: screenHeight,
-                            ),
-                            _buildMenuItem(
-                              title: 'Animali',
-                              imageUrl: 'assets/animal_icon.png',
-                              isLocked: true,
-                              points: 350,
-                              screenWidth: screenWidth,
-                              screenHeight: screenHeight,
-                            ),
-                            _buildMenuItem(
-                              title: 'Cibi',
-                              imageUrl: 'assets/food_icon.png',
-                              isLocked: true,
-                              points: 350,
-                              screenWidth: screenWidth,
-                              screenHeight: screenHeight,
-                            ),
-                            _buildMenuItem(
-                              title: 'Svago',
-                              imageUrl: 'assets/tv_icon.png',
-                              isLocked: true,
-                              points: 350,
-                              screenWidth: screenWidth,
-                              screenHeight: screenHeight,
-                            ),
-                          ],
-                        ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
                       ),
-                    ],
+                      elevation: 0,
+                      shadowColor: Colors.transparent,
+                    ),
+                    onPressed: () {
+                      // Add navigation logic here
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const RegistrationScreen()),
+                      );
+                    },
+                    child: Text(
+                      'Inizia Ora',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenWidth * 0.045,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                 ),
-              );
-            },
+              ),
+              
+              SizedBox(height: screenHeight * 0.02),
+              
+              // Login Text Button
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginScreen()),
+                  );
+                },
+                child: AppColors.gradientText('Login', screenWidth * 0.04),
+              ),
+              
+            ],
           ),
         ),
-      ),
-    );
-  }
-
-  Widget _buildMenuItem({
-    required String title,
-    required String imageUrl,
-    bool isLocked = false,
-    int points = 0,
-    required double screenWidth,
-    required double screenHeight,
-  }) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 16),
-      decoration: BoxDecoration(
-        color: AppColors.containerOpaqueColor,
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          Positioned(
-            left: -screenWidth * 0.04,
-            top: -screenHeight * 0.11,
-            child: Image.asset(
-              imageUrl,
-              width: screenWidth * 0.28,
-              height: screenHeight * 0.28,
-            ),
-          ),
-          ListTile(
-            contentPadding: const EdgeInsets.fromLTRB(100, 10, 20, 10),
-            title: AppColors.gradientText(title, screenWidth * 0.048),
-            trailing: isLocked
-                ? Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      CustomPaint(
-                          size: Size(screenWidth * 0.04, screenWidth * 0.04),
-                          painter: GradientIconPainter(
-                            icon: Icons.person,
-                            gradient: AppColors.textGradient,
-                          )),
-                      const SizedBox(width: 8),
-                      Text(
-                        'Costo: $points punti',
-                        style: TextStyle(
-                          fontSize: screenWidth * 0.03,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  )
-                : null,
-            onTap: () {},
-          ),
-        ],
       ),
     );
   }
